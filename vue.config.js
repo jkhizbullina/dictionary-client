da0cb718d187894354569e4f73fdc6f9c16f7258
jkhizbullina <jkhizbullina@gmail.com>
2023-10-06 00:00:46 +0500

    Добавление страниц

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: 'src/home/main.js',
+    home: 'src/home/main.js',
+    add_word: 'src/add_word/main.js',
+    words_list: 'src/words_list/main.js'
+  }
 })
